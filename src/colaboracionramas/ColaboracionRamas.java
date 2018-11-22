5e0586c98ae2b701fdb4552e5be75048a284375b
aperezcesar <aperezcesar@nautilus1.danielcastelao.org>
2018-11-22 09:51:23 +0100

    Commit conflicto

--- src/colaboracionramas/ColaboracionRamas.java
+++ src/colaboracionramas/ColaboracionRamas.java
@@ -5,6 +5,12 @@ public class ColaboracionRamas {
 
     public static void main(String[] args) {
         //Commit rama principal
+        //Commit 1
+        //Commit 2
+        //Commit 3
+        //Commit 4
+        //Commit 5
+        //Commit 6
     }
     
 }
