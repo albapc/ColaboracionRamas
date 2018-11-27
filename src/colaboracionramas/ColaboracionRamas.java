d67c81f91339926a03a106756549803dfd00e0f0
apinodominguez <apinodominguez@nautilus3.danielcastelao.org>
2018-11-27 09:18:39 +0100

    Commit para pull request. Rama secundaria

--- src/colaboracionramas/ColaboracionRamas.java
+++ src/colaboracionramas/ColaboracionRamas.java
@@ -5,6 +5,9 @@ public class ColaboracionRamas {
 
     public static void main(String[] args) {
         //Commit rama principal
+        int x = 7;
+        int y = 9;
+        int suma = x+y;
     }
     
 }
